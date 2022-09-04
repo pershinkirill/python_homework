a2f5b354ec606fbe88d2350f197f264a1917fa79
kirill <pershinkirill.n@gmail.com>
2022-09-04 17:16:44 +0300

    the thruth tables were corrected

--- task#2.py
+++ task#2.py
@@ -3,28 +3,28 @@ a, b, c = int(input("enter X: ")), int(input("enter Y: ")), int(input("enter Z: 
 
 #The left part of the equation
 left = not (a or b or c)
-##The truth table for the left part of the equation
-# X	 Y	Z	X ⋁ Y ⋁ Z	¬(X ⋁ Y ⋁ Z)
-# 0	 0	0	    0	          1
-# 0	 0	1	    1	          0
-# 0	 1	0	    1	          0
-# 0	 1	1	    1	          0
-# 1	 0	0	    1	          0
-# 1	 0	1	    1	          0
-# 1	 1	0	    1	          0
-# 1	 1	1	    1	          0
+#The truth table for the left part of the equation
+# X	| Y	| Z	| X ⋁ Y ⋁ Z	|¬(X ⋁ Y ⋁ Z)
+# 0	| 0	| 0	|     0	    |      1
+# 0	| 0	| 1	|     1	    |      0
+# 0	| 1	| 0	|     1	    |      0
+# 0	| 1	| 1	|     1	    |      0
+# 1	| 0	| 0	|     1	    |      0
+# 1	| 0	| 1	|     1	    |      0
+# 1	| 1	| 0	|     1	    |      0
+# 1	| 1	| 1	|     1	    |      0
 
 #The right part of the equation
 right = not a and not b and not c
 #The truth table for the right part of the equation
-# X	 Y	Z	¬X	¬Y	¬Z	¬X ⋀ ¬Y ⋀ ¬Z
-# 0	 0	0	 1	 1	 1	      1
-# 0	 0	1	 1	 1	 0	      0
-# 0	 1	0	 1	 0	 1	      0
-# 0	 1	1	 1	 0	 0	      0
-# 1	 0	0	 0	 1	 1	      0
-# 1	 0	1	 0	 1	 0	      0
-# 1	 1	0	 0	 0	 1	      0
-# 1	 1	1	 0	 0	 0	      0
+# X	| Y	| Z	|¬X	|¬Y	|¬Z	|¬X ⋀ ¬Y ⋀ ¬Z
+# 0	| 0	| 0	| 1	| 1	| 1	|      1
+# 0	| 0	| 1	| 1	| 1	| 0	|      0
+# 0	| 1	| 0	| 1	| 0	| 1	|      0
+# 0	| 1	| 1	| 1	| 0	| 0	|      0
+# 1	| 0	| 0	| 0	| 1	| 1	|      0
+# 1	| 0	| 1	| 0	| 1	| 0	|      0
+# 1	| 1	| 0	| 0	| 0	| 1	|      0
+# 1	| 1	| 1	| 0	| 0	| 0	|      0
 
 print(left == right)

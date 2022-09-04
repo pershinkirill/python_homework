84da201369b67b3b7ea2cf4871da9be886ee4ea1
kirill <pershinkirill.n@gmail.com>
2022-09-04 17:12:39 +0300

    thruth tables were added

--- task#2.py
+++ task#2.py
@@ -1,7 +1,30 @@
 # Напишите программу для проверки истинности утверждения ¬(X ⋁ Y ⋁ Z) = ¬X ⋀ ¬Y ⋀ ¬Z для всех значений предикат.
 a, b, c = int(input("enter X: ")), int(input("enter Y: ")), int(input("enter Z: "))
 
+#The left part of the equation
 left = not (a or b or c)
+##The truth table for the left part of the equation
+# X	 Y	Z	X ⋁ Y ⋁ Z	¬(X ⋁ Y ⋁ Z)
+# 0	 0	0	    0	          1
+# 0	 0	1	    1	          0
+# 0	 1	0	    1	          0
+# 0	 1	1	    1	          0
+# 1	 0	0	    1	          0
+# 1	 0	1	    1	          0
+# 1	 1	0	    1	          0
+# 1	 1	1	    1	          0
+
+#The right part of the equation
 right = not a and not b and not c
+#The truth table for the right part of the equation
+# X	 Y	Z	¬X	¬Y	¬Z	¬X ⋀ ¬Y ⋀ ¬Z
+# 0	 0	0	 1	 1	 1	      1
+# 0	 0	1	 1	 1	 0	      0
+# 0	 1	0	 1	 0	 1	      0
+# 0	 1	1	 1	 0	 0	      0
+# 1	 0	0	 0	 1	 1	      0
+# 1	 0	1	 0	 1	 0	      0
+# 1	 1	0	 0	 0	 1	      0
+# 1	 1	1	 0	 0	 0	      0
 
 print(left == right)

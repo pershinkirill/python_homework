7fc89af702fedb745b6998581a5f881834b0618e
kirill <pershinkirill.n@gmail.com>
2022-09-06 15:23:00 +0300

    third task - done

--- Seminar-2/task#3.py
+++ Seminar-2/task#3.py
@@ -1,5 +1,12 @@
 # Задайте список из k чисел последовательности (1 + 1\k)^k и выведите на экран их сумму.
 
 k = int(input("Enter K number: "))
+total = 0
 
+for i in range(1, k + 1):
+    print(f"{i} : {round((1 + 1/i)**i, 2)}", end=" ")
+    total += (1 + 1 / i) ** i
 
+print(end="\n")
+
+print(f"Sum of the values - {round(total, 2)}")

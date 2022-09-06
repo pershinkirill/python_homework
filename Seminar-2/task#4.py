dcbb8a6630443f317966ccb96a1baebef225a645
kirill <pershinkirill.n@gmail.com>
2022-09-06 17:06:49 +0300

    Forth task - completed

--- Seminar-2/task#4.py
+++ Seminar-2/task#4.py
@@ -1,3 +1,20 @@
 # Задайте список из N элементов, заполненных числами из промежутка [-N, N].
 # Найдите произведение элементов на указанных позициях. Позиции хранятся в файле file.txt в одной строке одно число.
 
+from numpy.random import randint #importing random integers generator
+
+n = int(input("Entre N number: "))
+
+rand_list = randint(-n, n, n)
+print(f"Random list in range from {-n} to {n} {rand_list}")
+
+text = open("file.txt", "r")
+text = text.readlines()
+total = 1
+i = 0
+
+while i < len(text):
+    total *= int(rand_list[int(text[i])])
+    i += 1
+
+print(total)

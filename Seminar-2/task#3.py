dff741655a4eb2eab502bcad9b73189864b6ae5a
kirill <pershinkirill.n@gmail.com>
2022-09-06 15:43:40 +0300

    third task updated

--- Seminar-2/task#3.py
+++ Seminar-2/task#3.py
@@ -2,11 +2,13 @@
 
 k = int(input("Enter K number: "))
 total = 0
+array = []
 
 for i in range(1, k + 1):
-    print(f"{i} : {round((1 + 1/i)**i, 2)}", end=" ")
+    # array.append(round((1 + 1/i)**i, 2))
+    print(f"{i} : {round((1 + 1/i)**i, 2)}", end=", ")
     total += (1 + 1 / i) ** i
 
 print(end="\n")
-
+# print(array)
 print(f"Sum of the values - {round(total, 2)}")

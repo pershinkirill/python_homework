4eed6239ff2f6db61fb9cde16a7a41a446a2efa5
kirill <pershinkirill.n@gmail.com>
2022-09-15 23:31:56 +0300

    third task - DONE

--- Seminar-4/task#3.py
+++ Seminar-4/task#3.py
@@ -1,18 +1,14 @@
 # Задайте последовательность чисел. Напишите программу, которая выведет список неповторяющихся
 # элементов исходной последовательности. [1, 1, 2, 3, 4, 5, 5] -> [2, 3, 4]
 
-lst = [1, 1, 2, 3, 4, 5, 5] #input('Please enter a list of numbers: ').split()
+lst = input('Please enter a list of numbers: ').split()
 res = []
+temp = []
 
-for i in lst:
-    count = 0
-    if i in lst:
-        print(i)
-        count += 1
-        print(count)
-        if count > 1:
-            continue
-        else:
-            res.append(i)
+for i in range(len(lst)):
+    if (lst[i] in lst[i + 1:]) or (lst[i] in temp):
+        temp.append(lst[i])
+    else:
+        res.append(int(lst[i]))
 
 print(res)

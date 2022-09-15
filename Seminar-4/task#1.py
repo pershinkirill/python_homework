d2ca3d4856adbbc1ec48ad2831b0db24e75375d0
kirill <pershinkirill.n@gmail.com>
2022-09-16 00:21:26 +0300

    First task - DONE

--- Seminar-4/task#1.py
+++ Seminar-4/task#1.py
@@ -1,15 +1,7 @@
 # Вычислить число c заданной точностью d
 # Пример: - при $d = 0.001, π = 3.141.$    $10^{-1} ≤ d ≤10^{-10}$
 
-import math
+a = int(input('Please enter a number: '))
+d = int(input('Please enter the number for accuracy selection : '))
 
-eps = 0.0001
-s = 0
-s1 = 1
-x = int(input(' '))
-k = 0
-while abs(s1 - s)> eps:
-    s1 = pow(x, k)/math.factorial(k)
-    s += s1
-    k += 1
-    print(float(x))
+print(f'{a:.{d}f}')

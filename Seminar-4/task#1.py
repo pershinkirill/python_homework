57274620a8cafb9c107f143e20907ec273a3c9d2
kirill <pershinkirill.n@gmail.com>
2022-09-16 00:33:40 +0300

    the first task was updated

--- Seminar-4/task#1.py
+++ Seminar-4/task#1.py
@@ -2,6 +2,13 @@
 # Пример: - при $d = 0.001, π = 3.141.$    $10^{-1} ≤ d ≤10^{-10}$
 
 a = int(input('Please enter a number: '))
-d = int(input('Please enter the number for accuracy selection : '))
+d = input('Please enter the number for accuracy selection : ')
+
+if float(d) < 1:
+    count = 0
+    while float(d) < 1:
+        d = float(d) * 10
+        count += 1
+    d = count
 
 print(f'{a:.{d}f}')

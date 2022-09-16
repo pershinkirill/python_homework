1353b3cfd957756841760638706c7e7e3bc64f58
kirill <pershinkirill.n@gmail.com>
2022-09-16 10:24:58 +0300

    Fourth task with resulted .txt updated

--- Seminar-4/task#4.py
+++ Seminar-4/task#4.py
@@ -1,7 +1,7 @@
 # Задана натуральная степень k. Сформировать случайным образом список коэффициентов (значения от 0 до 100)
 # многочлена и записать в файл многочлен степени k. Пример: k=2 => 2*x² + 4*x + 5 = 0 или x² + 5 = 0 или 10*x² = 0
-import random
-import numpy
+import random # will be used for random coefficients generating
+import numpy # will be used for a a polynomial generation
 
 k = int(input("Please enter k number, that will be a degree of a polynomial: "))
 coeff = []
